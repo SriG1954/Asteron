b53894cbaeda9774fc3c39973f9cd8f296c19675
Srinivasan Govintharaju <srinivasan.govintharaju@resolutionlife.com.au>
2026-01-07 15:51:07 +1100

    commit three 7 jan 26

--- AppCoreV1/ABLEModels/AblearchivalContext.cs
+++ AppCoreV1/ABLEModels/AblearchivalContext.cs
@@ -17,7 +17,7 @@ public partial class AblearchivalContext : DbContext
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=claimscentral-sql.wlife.com.au;Database=ABLEArchival;User ID=db_admin;Password=90K8mKX23jigVLesL40d;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
